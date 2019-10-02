fb2ca2b645e48f3ec1ace65732833eb7f892b0fe
Jonathan Cran <jcran@pentestify.com>
2019-10-02 16:06:18 -0500

    add socket timeout back

--- lib/chrome_remote/socket.rb
+++ lib/chrome_remote/socket.rb
@@ -4,20 +4,21 @@ module ChromeRemote
   class Socket
     attr_reader :url, :io
 
-    def initialize(url)
+    def initialize(url, timeout=30)
       uri = URI.parse(url)
-
+      @timeout = timeout
       @url = url
-      @socket = Socketry::TCP::Socket.connect(uri.host, uri.port, timeout: 30)
+      @socket = Socketry::TCP::Socket.connect(uri.host, uri.port, timeout: @timeout)
       @io = @socket.to_io
     end
     
     def write(data)
-      io.print data
+      @socket.write data
     end
 
     def read
-      io.readpartial(1024)
+      @socket.readpartial(1024, timeout: @timeout)
     end
+    
   end
 end
